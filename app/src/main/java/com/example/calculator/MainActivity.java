d938080f7a681bea5f4c48cab37425afd5aad934
cnon06 <sinanalagoz74@gmail.com>
2020-10-20 20:54:54 +0300

    v4

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView display;
+
+    Button one,two,three,clear;
+
+    String dsp="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display = (TextView) findViewById(R.id.display);
+
+        one = (Button) findViewById(R.id.one);
+        clear = (Button) findViewById(R.id.clear);
+
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               if(dsp=="0") dsp="";
+                dsp+="1";
+                display.setText(dsp);
+                        }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dsp="0";
+                display.setText(dsp);
+            }
+        });
+
     }
 }
