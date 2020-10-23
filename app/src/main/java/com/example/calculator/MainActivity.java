fa93be67f898a160f7181ac662c5ad2413ad8ecb
cnon06 <sinanalagoz74@gmail.com>
2020-10-23 11:21:39 +0300

    v7

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -17,7 +17,16 @@ public class MainActivity extends AppCompatActivity {
     String dsp="0";
     Integer number_one=0,number_two=0;
 
-    boolean sub_cont=true;
+    boolean sub_cont=true,add_cont=true;
+
+
+    public void numbers(String number)
+    {
+        if(dsp=="0") dsp="";
+        dsp+=number;
+        display.setText(dsp);
+        add_cont=true;
+    }
 
 
     @Override
@@ -83,95 +92,138 @@ public class MainActivity extends AppCompatActivity {
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // if(dsp=="0") dsp="";
-               // dsp+="1";
-               number_one+=Integer.parseInt(display.getText().toString());
 
-               //display.setText("");
-                dsp="";
-                display.setText(number_one+"");
+                if(add_cont)
+                {
+                    number_one+=Integer.parseInt(display.getText().toString());
+                    dsp="";
+                    display.setText(number_one+"");
+                    add_cont=false;
+                }
+
             }
         });
+
+
+
 
 
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               if(dsp=="0") dsp="";
+
+                numbers("1");
+                /*
+                   if(dsp=="0") dsp="";
                 dsp+="1";
                 display.setText(dsp);
+                 */
+
                         }
         });
 
         two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(dsp=="0") dsp="";
+
+             numbers("2");
+             /*
+               if(dsp=="0") dsp="";
                 dsp+="2";
                 display.setText(dsp);
+              */
+
             }
         });
 
         three.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(dsp=="0") dsp="";
+                numbers("3");
+                /*
+                  if(dsp=="0") dsp="";
                 dsp+="3";
                 display.setText(dsp);
+                 */
+
             }
         });
 
         four.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(dsp=="0") dsp="";
+                numbers("4");
+                /*
+                   if(dsp=="0") dsp="";
                 dsp+="4";
                 display.setText(dsp);
+                 */
+
             }
         });
 
         five.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                numbers("5");
+               /*
                 if(dsp=="0") dsp="";
                 dsp+="5";
                 display.setText(dsp);
+                */
+
             }
         });
 
         six.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(dsp=="0") dsp="";
+                numbers("6");
+                /*
+                 if(dsp=="0") dsp="";
                 dsp+="6";
                 display.setText(dsp);
+                 */
+
             }
         });
 
         seven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+               numbers("7");
+               /*
                 if(dsp=="0") dsp="";
                 dsp+="7";
                 display.setText(dsp);
+                */
+
             }
         });
 
         eight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(dsp=="0") dsp="";
+               numbers("8");
+               /*
+                 if(dsp=="0") dsp="";
                 dsp+="8";
                 display.setText(dsp);
+                */
+
             }
         });
 
         nine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(dsp=="0") dsp="";
+
+                numbers("9");
+                /*
+                  if(dsp=="0") dsp="";
                 dsp+="9";
                 display.setText(dsp);
+                 */
             }
         });
 
