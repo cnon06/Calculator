e08e8d4cb3ae52395ebe8f570abd613ff80dbb50
sinan <sinanalagoz74@gmail.com>
2021-04-22 20:07:13 +0300

    17

--- app/src/main/java/com/calculator2/calculator/MainActivity.java
+++ app/src/main/java/com/calculator2/calculator/MainActivity.java
@@ -2,9 +2,14 @@ package com.calculator2.calculator;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.drawable.DrawableCompat;
 
 import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -356,13 +361,40 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+              divide.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        divide.setBackgroundColor(Color.RED);
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        divide.setBackgroundColor(Color.parseColor("#17615B"));
+                        process("/");
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
+
+/*
+
         divide.setOnClickListener(new View.OnClickListener() {
             @Override
+
+
+
             public void onClick(View v) {
                 process("/");
 
             }
         });
+ */
+
+
+
 
 
 
